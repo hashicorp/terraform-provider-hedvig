2e821dcdcdc09ea6d8f074f775d42059f14ae5ee
katrinahayes <katrina@hedviginc.com>
2019-05-23 13:59:46 -0700

    Remove cluster parameter from example file

--- examples/config.tf
+++ examples/config.tf
@@ -5,40 +5,34 @@ provider "hedvig" {
 }
 
 resource "hedvig_vdisk" "my-vdisk" {
-  cluster = "tfhashicorp"
   name = "HedvigVdiskNN"
   size = 18
   type = "BLOCK"
 }
 
 resource "hedvig_lun" "my-lun3" {
-  cluster = "tfhashicorp3"
   vdisk = "${hedvig_vdisk.my-vdisk.name}"
   controller = "tfhashicorpvip1.external.hedviginc.com"
 }
 
 resource "hedvig_vdisk" "my-vdisk2" {
-  cluster = "tfhashicorp"
   name = "HedvigVdiskOO"
   size = 20
   type = "NFS"
 }
 
 #resource "hedvig_vdisk" "my-vdisk3" {
-#  cluster = "tfhashicorp"
 #  name = "HedvigVdiskC"
 #  size = 22
 #  type = "NFS"
 #}
 
 resource "hedvig_mount" "my-mount" {
-  cluster = "tfhashicorp3"
   vdisk = "${hedvig_vdisk.my-vdisk2.name}"
   controller = "tfhashicorpvip1.external.hedviginc.com"
 }
 
 #resource "hedvig_access" "my-access" {
-#  cluster = "tfhashicorp"
 #  vdisk = "${hedvig_vdisk.my-vdisk.name}"
 #  host = "${hedvig_lun.my-lun3.controller}"
 #  address = "172.22.22.21"
@@ -46,7 +40,6 @@ resource "hedvig_mount" "my-mount" {
 #}
 
 #resource "hedvig_access" "my-access2" {
-#  cluster = "tfhashicorp"
 #  vdisk = "${hedvig_vdisk.my-vdisk.name}"
 #  host = "${hedvig_lun.my-lun3.controller}"
 #  address = "172.22.22.25"
@@ -54,7 +47,6 @@ resource "hedvig_mount" "my-mount" {
 #}
 
 #resource "hedvig_access" "my-access3" {
-#  cluster = "tfhashicorp"
 #  vdisk = "${hedvig_vdisk.my-vdisk.name}"
 #  host = "${hedvig_lun.my-lun3.controller}"
 #  address = "172.22.22.30"
@@ -62,7 +54,6 @@ resource "hedvig_mount" "my-mount" {
 #}
 
 #resource "hedvig_access" "my-access4" {
-#  cluster = "tfhashicorp"
 #  vdisk = "${hedvig_vdisk.my-vdisk2.name}"
 #  host = "${hedvig_mount.my-mount.controller}"
 #  address = "172.22.22.31"
